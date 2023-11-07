212d954e9d30ab153eda13b0747d8e1aa26c102e
User <User@WIN-R1OMRL0S822>
2023-11-07 15:43:35 +0500

    Add project files.

---
+++ BookCafe.Service/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCafe.Service
+{
+    public class Menu
+    {
+        public string[,] listBooks = 
+        {
+            {"O'tgan kunlar", "40000","Abdullo Qodiriy","utgan asr haqida"},
+            {"Ufq", "60000","Said Ahmat","Urush haqida"},
+            {"Dunyoning ishlari", "30000","O'tkir Xoshimov","Dunyoning g'aroyib ishlari"},
+            {"Shum bola", "20000","G'ofur G'ulom","zur kitob"},
+        };
+
+
+    }
+}
