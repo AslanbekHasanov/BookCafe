7144621cfe51db9c795bfda70ecdcf16dd07685b
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-11-11 16:02:48 +0500

    CLEAR: All info and One info

---
+++ BookCafe.Service/ClearAllData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCafe.Service
+{
+    public class ClearAllData
+    {
+
+        Menu menu = new Menu();
+
+
+
+
+    }
+}
--- BookCafe.Service/Menu.cs
+++ BookCafe.Service/Menu.cs
@@ -8,14 +8,98 @@ namespace BookCafe.Service
 {
     public class Menu
     {
-        public string[,] listBooks = 
+
+        public string[,] listBooks =
         {
             {"O'tgan kunlar", "40000","Abdullo Qodiriy","utgan asr haqida"},
             {"Ufq", "60000","Said Ahmat","Urush haqida"},
-            {"Dunyoning ishlari", "30000","O'tkir Xoshimov","Dunyoning g'aroyib ishlari"},
-            {"Shum bola", "20000","G'ofur G'ulom","zur kitob"},
+            {"Dunyoning ishlari", "30000","O'tkir Xoshimov","Dunyoning g'aroyib ishlari"}
         };
 
+
+
+        public void IsDeleteInfo(string book_name)
+        {
+            bool isfound = false;
+            for (int i = 0; i < listBooks.Length / 4; i++)
+            {
+                for (int j = 0; j < 4; j += 3)
+                {
+                    if (listBooks[i, j] == book_name)
+                    {
+                        isfound = true;
+                        //code delete
+                        listBooks[i, j] = null;
+                        listBooks[i, (j + 1)] = null;
+                        listBooks[i, (j + 2)] = null;
+                        listBooks[i, (j + 3)] = null;
+
+
+                    }
+
+                }
+
+            }
+            if (isfound == false)
+            {
+                Console.WriteLine("Not Found");
+
+            }
+
+        }
+        public void GetAllInfo()
+        {
+            
+            for (int i = 0; i < listBooks.Length / 4; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < 4; j++)
+                {
+                    if (listBooks[i, j] != null )
+                    {
+                        Console.Write(listBooks[i, j] + "  ");
+
+                    }
+                    else
+                    {
+                        count++;
+                    }
+
+                }
+                if (count != 4)
+                {
+                    Console.WriteLine();                
+
+                }
+                
+
+            }
+
+        }
+
+        public void ClearAllInfo()
+        {
+            if (listBooks != null)
+            {
+                for (int i = 0; i < listBooks.Length / 4; i++)
+                {
+                    for (int j = 0; j < 4; j++)
+                    {
+                        listBooks[i, j] = null;
+
+
+                    }
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Baza mavjud emas");
+            }
+
+
+        }
 
     }
 }
